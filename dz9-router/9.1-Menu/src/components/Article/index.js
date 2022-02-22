81010699d6e058f102d0307c84710940d0c03e52
mea2k <69173130+mea2k@users.noreply.github.com>
2022-02-23 01:00:41 +0300

    Add files via upload

---
+++ dz9-router/9.1-Menu/src/components/Article/index.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types'
+
+import './main.css';
+
+const Article = ({ title, info }) => (
+    <article className="article">
+        <h1 className="article__title">{title}</h1>
+        {info && info.map((v, key) => (
+            <p className="article__paragraph" key={key}>
+                {v}
+            </p>
+        ))}
+    </article>
+);
+
+
+Article.propTypes = {
+    title: PropTypes.string,
+    info: PropTypes.array
+};
+
+
+export default Article;
---
+++ dz9-router/9.1-Menu/src/components/Menu/index.js
@@ -0,0 +1,27 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom';
+
+import './main.css';
+
+const Menu = ({ data }) => (
+    <nav className="menu">
+        {data && data.map((v) => (
+            <NavLink
+                to={v.url}
+                key={v.key}
+                className={({ isActive }) => 'menu__item ' + (isActive ? 'menu__item-active' : '')}
+            >
+                {v.title}
+            </NavLink>
+        ))}
+    </nav>
+);
+
+
+Menu.propTypes = {
+    data: PropTypes.array,
+};
+
+
+export default Menu;
